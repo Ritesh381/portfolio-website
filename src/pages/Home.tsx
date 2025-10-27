c5e5de9795c17d9ff78f5205d79d4260f5225981
Ritesh Prajapati <prajapatiritesh381@gmail.com>
2025-10-27 12:31:51 +0530

    Updates in projects sectoin

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import me from "../assets/me.png";
 
 function Home() {
   const about =
-    "Hi, I'm Ritesh Prajapati — a computer science student passionate about competitive programming, full-stack development, and AI. I enjoy solving tough problems and turning ideas into real projects, from web apps to AI-powered tools. Always curious and eager to learn, I'm working towards building impactful products that people love to use.";
+    "Hi, I'm Ritesh Prajapati — a computer science student passionate about competitive programming, full-stack development, and AI. I enjoy solving problems and turning ideas into real projects, from web apps to AI-powered tools. Always curious and eager to learn, I'm working towards building impactful products that people love to use.";
   return (
     <div>
       <div className="min-h-[20vh] mt-20 lg:mt-30 xl:mt-30 flex flex-col items-center justify-around lg:flex-row xl:flex-row md:flex-row">
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,4 @@
-import ProjectCard from '../components/ProjectCard';
+import ProjectCard from "../components/ProjectCard";
 import FIARY from "../assets/projectPics/FIARY.png";
 import Notes from "../assets/projectPics/Notes.png";
 import MovieHub from "../assets/projectPics/MovieHub.png";
@@ -9,52 +9,54 @@ import RS from "../assets/projectPics/RS.png";
 const projects = [
   {
     name: "FIARY",
-    desc: "A diary with a personality crisis — it talks back to you like your sassiest mirror. Still under construction 👷‍♂️",
+    desc: "FIARY is your personal life dashboard. It unites Journaling, Habits, Tasks, and Finance into one platform, using your data to provide actionable, holistic self-insights.",
     img: FIARY,
     github: "https://github.com/Ritesh381/FIARY",
+    demo: "https://fiary.vercel.app",
   },
   {
     name: "AI Powered Notes App",
-    desc: "Why write notes when AI can do it faster (and neater) than you? Study material in seconds, procrastination in minutes.",
+    desc: "Create notes and generate AI insights using those notes like Quiz, Flash Cards, Summary etc.",
     img: Notes,
     github: "https://github.com/Ritesh381/Simple-Notes",
     demo: "https://smpl-notes.vercel.app/",
   },
   {
     name: "Movie Hub",
-    desc: "A paradise for movie nerds 🍿. Stalk films, explore trailers, and chat with PopcornPilot — your overly dramatic AI movie buddy.",
+    desc: "Surf and explore trending movies, Watch trailers, read about the movie and Chat with Porcorn Piolot an AI ChatBOT which tells you about movies.",
     img: MovieHub,
     github: "https://github.com/Ritesh381/Simple-Notes",
     demo: "https://movie-hub404.vercel.app/",
   },
   {
     name: "School Website",
-    desc: "Built this for my school when I was 11. Spoiler: it’s still better than some ‘professional’ websites out there 😂",
+    desc: "Built this for my school, when i was learning React.",
     img: School,
     github: "https://github.com/Ritesh381/School-website",
     demo: "https://rspublicschool.vercel.app/#courses",
   },
   {
     name: "Vending Machine",
-    desc: "A vending machine that takes QR payments. Sadly, it doesn’t dispense friendship or free WiFi (yet).",
+    desc: "Curiosity hit when I was using a vending machine and this came as result. The working of QR was a challenge but somehow using only HTML, JS I gave it a life.",
     img: VM,
     github: "https://github.com/Ritesh381/Vending-Machine",
     demo: "https://thesnackpoint.netlify.app/",
   },
   {
     name: "Random Stuff",
-    desc: "The internet’s junk drawer — share weird ideas, odd products, or just whatever you had for lunch. People will still look 👀",
+    desc: "This was my first project, I implemented CRUD operation in vanilla js using supabase.",
     img: RS,
     github: "https://github.com/Ritesh381/Random-Stuff",
     demo: "https://random-stuff-01.netlify.app/",
   },
 ];
 
-
 function Projects() {
   return (
     <div className="container mx-auto p-4 md:p-8">
-      <h2 className="text-4xl font-bold text-center text-white mb-10">My Projects</h2>
+      <h2 className="text-4xl font-bold text-center text-white mb-10">
+        My Projects
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects.map((project, index) => (
           <ProjectCard
